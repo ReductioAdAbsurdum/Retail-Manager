314be4771aa5612e5cf0dff670d2f5d249d62826
SergioStankovik <moonburn.co@gmail.com>
2022-01-22 11:24:25 +0100

    Add WPF Desktop UI
    Adds WPF project, using CaliburnMicro and bootstrapper creates MVVM template

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
